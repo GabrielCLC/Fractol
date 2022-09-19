8ffac7ebcfeefb3df29f159502a5e930872bc05d
gcorreia <gcorreia@student.42.rio>
2022-09-19 14:14:02 -0300

    added argument validation message

--- fractol.h
+++ fractol.h
@@ -10,7 +10,7 @@
 # define BLUE "\033[1;34m"
 # define YLLW "\033[1;33m"
 # define GREEN "\033[1;32m"
-# define PURPL "\033[0;35m"
+# define PURPL "\033[1;35m"
 # define RESET "\033[0m"
 # endif
 
--- validation.c
+++ validation.c
@@ -6,7 +6,7 @@
 /*   By: gcorreia <gcorreia@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 12:56:46 by gcorreia          #+#    #+#             */
-/*   Updated: 2022/09/13 18:39:10 by gcorreia         ###   ########.fr       */
+/*   Updated: 2022/09/19 14:12:23 by gcorreia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@
 static void	print_error()
 {
 	ft_printf(RED"\n---Invalid argument---\n\n"RESET);
-	ft_printf("the arguments should be as follows:\n\n");
-	ft_printf("./fractol "BLUE"<Name> "YLLW"<Iteration limit> "
-			GREEN"<Window size> ");
-	ft_printf("<Real part of the constant> <Imaginary part of the constant>\n\n");
-	ft_printf("examples:\n");
-	ft_printf("./fractol mandelbrot 500 800\n");
-	ft_printf("./fractol mandelbrot 1000 400\n");
-	ft_printf("./fractol julia 700 600 0 1\n");
-	ft_printf("./fractol julia 500 500 -0.75 0\n");
-	ft_printf("./fractol julia 400 800 -1 0\n");
-	ft_printf("./fractol julia 1000 800 0.3 0.6\n");
+	ft_printf("The arguments should be as follows:\n\n");
+	ft_printf(GREEN"./fractol "BLUE"<Name> "YLLW"<Iteration limit> "
+			"<Window size> "PURPL "<Real part of the constant>"
+		   	"<Imaginary part of the constant>\n\n"RESET);
+	ft_printf("Examples:\n\n");
+	ft_printf(GREEN"./fractol\t"BLUE"mandelbrot\t"YLLW"500  800\n");
+	ft_printf(GREEN"./fractol\t"BLUE"mandelbrot\t"YLLW"1000 400\n");
+	ft_printf(GREEN"./fractol\t"BLUE"julia\t\t"YLLW"700  600\t"PURPL
+			" 0    1\n");
+	ft_printf(GREEN"./fractol\t"BLUE"julia\t\t"YLLW"500  500\t"PURPL
+			"-0.75 0\n");
+	ft_printf(GREEN"./fractol\t"BLUE"julia\t\t"YLLW"400  800\t"PURPL
+			"-1    0\n");
+	ft_printf(GREEN"./fractol\t"BLUE"julia\t\t"YLLW"1000 1000\t"PURPL
+			" 0.3  0.6\n");
+	ft_printf(RESET"\n");
 }
 
 void	check_render_options(char	**argv)
